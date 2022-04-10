890bedd94f6a9e20f1016ac76f746757fd07cbfa
LDeepakReddy <l.deepakreddy.com>
2022-04-10 13:36:57 +0530

    ADDED:functionality to read and write data from console

--- src/main/java/com/blz/employeepayrollservice/model/EmployeePayRoll.java
+++ src/main/java/com/blz/employeepayrollservice/model/EmployeePayRoll.java
@@ -1,5 +1,52 @@
 package com.blz.employeepayrollservice.model;
 
 public class EmployeePayRoll {
+        private int id;
+        private String name;
+        private int salary;
+
+        public EmployeePayRoll() {
+
+        }
+
+        public EmployeePayRoll(int id, String name, int salary) {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setSalary(int salary) {
+            this.salary = salary;
+        }
+
+        @Override
+        public String toString() {
+            return "EmployeePayroll{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", salary=" + salary +
+                    '}';
+        }
+
 
 }
--- src/main/java/com/blz/employeepayrollservice/service/EmployeePayRollService.java
+++ src/main/java/com/blz/employeepayrollservice/service/EmployeePayRollService.java
@@ -1,8 +1,33 @@
 package com.blz.employeepayrollservice.service;
 
-public class EmployeePayRollService {
-    public static void main(String[] args){
-        System.out.println("Welcome to Employee Payroll service program");
+import com.blz.employeepayrollservice.model.EmployeePayRoll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayRollService{
+    Scanner scanner = new Scanner(System.in);
+    List<EmployeePayRoll> employeePayrolls = new ArrayList<>();
+
+    public static void main(String[] args) {
+        EmployeePayRollService service = new EmployeePayRollService();
+        service.readData();
+        service.writeData();
     }
 
+    private void readData() {
+        EmployeePayRoll employeePayroll = new EmployeePayRoll();
+        System.out.println("Enter Id");
+        employeePayroll.setId(scanner.nextInt());
+        System.out.println("Enter Name");
+        employeePayroll.setName(scanner.next());
+        System.out.println("Enter Salary");
+        employeePayroll.setSalary(scanner.nextInt());
+        employeePayrolls.add(employeePayroll);
+    }
+
+    private void writeData() {
+        System.out.println(employeePayrolls);
+    }
 }
